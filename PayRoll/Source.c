fa365dfc34efc098c4f995be84973262e4cd54fe
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-10-28 13:22:06 +0100

    Add project files.

---
+++ PayRoll/Source.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//Main
+void main()
+{
+	// Declare variables
+	int employNum, hours, payRate, sales;
+
+	//Ask User for first Number
+	printf("Please enter employee number:\t");
+	scanf("%d", &employNum);
+
+	//Until -1 is entered
+	while (employNum > 0)
+	{
+		
+
+		//Ask for Employee Number
+		printf("Please enter employee number:\t");
+		scanf("%d", &employNum);
+
+		//Ask for Employee Number of hours
+		printf("Please enter number of hours worked:\t");
+		scanf("%d", &hours);
+	}
+}
